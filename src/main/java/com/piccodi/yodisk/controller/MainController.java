e061fbb2db7503d3377dd0f360bb492f91f3440d
piccodi <akakoa@yandex.ru>
2022-02-12 20:41:06 +0300

    link generation in progress

--- src/main/java/com/piccodi/yodisk/controller/MainController.java
+++ src/main/java/com/piccodi/yodisk/controller/MainController.java
@@ -1,11 +1,8 @@
 package com.piccodi.yodisk.controller;
 
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/")
